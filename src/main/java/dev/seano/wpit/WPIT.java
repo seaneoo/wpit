43ee498cbf2d5c4847c9586c1b12e76a36188237
Sean O'Connor <so@seano.dev>
2022-01-13 10:30:56 -0700

    Setup mod basics

---
+++ src/main/java/dev/seano/wpit/WPIT.java
@@ -0,0 +1,11 @@
+package dev.seano.wpit;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WPIT implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/seano/wpit/client/WPITClient.java
@@ -0,0 +1,14 @@
+package dev.seano.wpit.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class WPITClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
