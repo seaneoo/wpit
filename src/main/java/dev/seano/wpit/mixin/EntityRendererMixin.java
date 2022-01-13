42a54eaca78505c4e541f742c6edb404e94a447d
Sean O'Connor <so@seano.dev>
2022-01-13 13:06:03 -0700

    Add EntityRendererMixin.java

---
+++ src/main/java/dev/seano/wpit/mixin/EntityRendererMixin.java
@@ -0,0 +1,25 @@
+package dev.seano.wpit.mixin;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.HorseBaseEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Environment(EnvType.CLIENT)
+@Mixin(EntityRenderer.class)
+public class EntityRendererMixin<T extends Entity> {
+
+    // Render a nameplate for the name of the entity's owner
+    @Inject(method = {"render"}, at = {@At(value = "HEAD")})
+    private void render(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        // TODO
+    }
+}
