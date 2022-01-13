276324c6e9c3b2596bb98cab6c55f2601bdd7067
Sean O'Connor <so@seano.dev>
2022-01-13 13:46:11 -0700

    Add public/private fields to WPIT.java

--- src/main/java/dev/seano/wpit/WPIT.java
+++ src/main/java/dev/seano/wpit/WPIT.java
@@ -3,12 +3,28 @@ package dev.seano.wpit;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Environment(EnvType.CLIENT)
 public class WPIT implements ClientModInitializer {
 
+    public static final String ID = "wpit";
+
+    private static WPIT INSTANCE;
+    private Logger logger;
+
     @Override
     public void onInitializeClient() {
+        INSTANCE = this;
+        logger = LogManager.getLogger(ID);
+    }
 
+    public static WPIT getInstance() {
+        return INSTANCE;
+    }
+
+    public Logger getLogger() {
+        return logger;
     }
 }
