0d2e1e84efc26eb3c3ffc2786aaa36be0d8fde5f
Sean O'Connor <so@seano.dev>
2023-02-19 19:33:19 -0700

    Add Javadoc comments

--- src/main/java/dev/seano/wpit/mixin/FoxEntityInvoker.java
+++ src/main/java/dev/seano/wpit/mixin/FoxEntityInvoker.java
@@ -22,9 +22,15 @@ import java.util.UUID;
 @Mixin(FoxEntity.class)
 public interface FoxEntityInvoker {
 
+    /**
+     * Allows the {@link FoxEntity#OWNER} tracked data to be accessed.
+     */
     @Accessor("OWNER")
     TrackedData<Optional<UUID>> getOwnerData();
 
+    /**
+     * Allows the {@link FoxEntity#OTHER_TRUSTED} tracked data to be accessed.
+     */
     @Accessor("OTHER_TRUSTED")
     TrackedData<Optional<UUID>> getOtherTrustedData();
 }
