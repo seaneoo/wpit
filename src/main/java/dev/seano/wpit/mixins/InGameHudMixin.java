b068eb5a8fb2eb56a37b1998ea0bba0797fa4201
Sean O'Connor <so@seano.dev>
2023-08-25 21:28:43 -0600

    Improve InGameHudMixin

--- src/main/java/dev/seano/wpit/mixins/InGameHudMixin.java
+++ src/main/java/dev/seano/wpit/mixins/InGameHudMixin.java
@@ -5,8 +5,8 @@ import dev.seano.wpit.hud.Tooltip;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,27 +16,35 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Optional;
+
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
     @Shadow
     @Final
     private MinecraftClient client;
 
+    /**
+     * If our HUD can be displayed. Based upon the game HUD being hidden or the debug menu being open.
+     *
+     * @return True, if we should display the HUD.
+     */
     @Unique
-    private Tooltip tooltip;
-
-    @Inject(method = "<init>", at = @At("TAIL"))
-    public void wpit$init(MinecraftClient client, ItemRenderer itemRenderer, CallbackInfo ci) {
-        this.tooltip = new Tooltip(client, Text.of("Apple")); // TODO: Get the entity data
+    public boolean canDisplay() {
+        return !this.client.options.debugEnabled && !this.client.options.hudHidden;
     }
 
     @Inject(method = "render", at = @At("TAIL"))
     public void wpit$render(DrawContext context, float tickDelta, CallbackInfo ci) {
-        if (!this.client.options.debugEnabled && !this.client.options.hudHidden) {
-            Entity entity = WPITMod.RAY_TRACING.target;
-            if (entity != null) {
+        if (canDisplay()) {
+            Optional<Entity> entity = WPITMod.RAY_TRACING.getTarget();
+            if (entity.isPresent() && entity.get() instanceof LivingEntity) {
                 this.client.getProfiler().push("wpit tooltip");
-                this.tooltip.render(context);
+
+                Text text0 = Text.translatable(entity.get().getType().getTranslationKey());
+                Text text1 = Text.of("Lorem Ipsum");
+                new Tooltip(client, text0, text1).render(context);
+
                 this.client.getProfiler().pop();
             }
         }
